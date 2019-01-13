175f0868e251c5f5e18788cb6ff4876e3e6698c8
swechchhaparajuli <swechchha@gmail.com>
2019-01-12 18:12:51 -0800

    shadows n stuff

--- javascript/runSite.js
+++ javascript/runSite.js
@@ -24,7 +24,15 @@ var listOfKids = [
     "name" : "Asish",
     "video" : "https://www.youtube.com/embed/jqEClhF2GZo",
     "image" : "./kidInfo/Asish/image.jpg",
-    "profile" : profileWriter(this.name)
+    "profile" : profileWriter("Name: Ashish" +
+
+        "Age: 10 years " + 
+        
+        "Future career: Doctor " +
+        
+        "Hobby: Play football " +
+        
+        "Favourite Food: Chicken curry")
 },
 
 {
@@ -38,63 +46,123 @@ var listOfKids = [
     "name" : "Laxman",
     "video" : "https://www.youtube.com/embed/iYH1Ji3YZBc",
     "image" : "./kidInfo/Laxman/image.jpg",
-    "profile" : profileWriter(this.name)
+    "profile" : profileWriter("Name: Laxman Bika" +
+
+    "Age: 10 years " +
+    
+    "Future career: Teacher " +
+    
+    "Hobby: Reading, playing, singing and dancing " +
+    
+    "Favourite Food: Cake")
 },
 
 {
     "name" : "Manika",
     "video" : "https://www.youtube.com/embed/XC5JfCI5Hu0",
     "image" : "./kidInfo/Manika/image.jpg",
-    "profile" : profileWriter(this.name)
+    "profile" : profileWriter("Name: Manika Bika " +
+
+    "Age: 8 years " + 
+    
+    "Future career: Teacher " +
+    
+    "Favourite Food: Momo")
 },
 
 {
     "name" : "Manisha",
     "video" : "https://www.youtube.com/embed/wCoWXgv5Yzc",
     "image" : "./kidInfo/Manisha/image.jpg",
-    "profile" : profileWriter(this.name)
+    "profile" : profileWriter("Name: Manisha Bika " +
+
+    "Age: 9 years " +
+    
+    "Favourite Food: Momo ")
 },
 
 {
     "name" : "Nishan",
     "video" : "https://youtube.com/embed/Wm7cBx6L8oU",
     "image" : "./kidInfo/Nishan/image.jpg",
-    "profile" : profileWriter(this.name)
+    "profile" : profileWriter("Name: Nishan" +
+
+    "Age: 11 years " +
+    
+    "Future career: Dancer " +
+    
+    "Hobby: Singing " +
+    
+    "Favourite Food: Chicken curry")
 },
 
 {
     "name" : "Ranju",
     "video" : "https://www.youtube.com/embed/5K0ymZ_KKq8",
     "image" : "./kidInfo/Ranju/image.jpg",
-    "profile" : profileWriter(this.name)
+    "profile" : profileWriter("Name: Ranju " +
+
+    "Age: 13 years " +
+    
+    "Future career: Scientist " +
+    
+    "Hobby: Badminton " +
+    
+    "Favourite Food: Chicken curry and Roti")
 },
 
 {
     "name" : "Santosh",
     "video" : "https://youtube.com/embed/zuEAOeDWee4",
     "image" : "./kidInfo/Santosh/image.jpg",
-    "profile" : profileWriter(this.name)
+    "profile" : profileWriter("Name: Santosh Rawal " +
+
+    "Age: 11 years " +
+    
+    "Future career: Pilot " +
+    
+    "Food: Chicken Momo")
 },
 
 {
     "name" : "Sudeep",
     "video" : "https://youtube.com/embed/I3thiGB_IUc",
     "image" : "./kidInfo/Sudeep/image.jpg",
-    "profile" : profileWriter(this.name)
+    "profile" : profileWriter("Name: Sudeep Pariyar " +
+
+    "Age: 10 years " +
+    
+    "Future career: Singer " +
+    
+    "Hobby: Dance and Drawing " +
+    
+    "Favourite Food: Cake with frosting")
 },
 
 {
     "name" : "Susmita",
     "video" : "https://www.youtube.com/embed/LAM67rWkJUU",
     "image" : "./kidInfo/Susmita/image.jpg",
-    "profile" : profileWriter(this.name)
+    "profile" : profileWriter("Name: Susmita " +
+
+    "Age: 11 years " +
+    
+    "Future career: Teacher " +
+    
+    "Hobby: Dance, reading and writing " +
+    
+    "Favourite Food: Chow Mein")
 },
 
 {
     "name" : "Tulsi",
     "video" : "https://www.youtube.com/embed/17JW-eM87bk",
     "image" : "./kidInfo/Tulsi/image.jpg",
-    "profile" : profileWriter(this.name)
+    "profile" : profileWriter("Name: Tulsi " +
+
+    "Age: 6 years " +
+    
+    "Food: Momo and noodles")
 }
 
 ];
